4d21be0f9af6a2125f625c02d12344fb867c0d90
Alon <kanter.alon@gmail.com>
2025-05-27 20:09:57 -0300

    Fix import path for CommunityPage and restructure Router in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,25 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import BlogPage from './pages/BlogPage';
-import CommunityPage from './pages/CommunityPage';
+import CommunityPage from './Pages/CommunityPage';
 import ProductPage from './pages/ProductPage';
 import LoginPage from './pages/Login';
 import SearchResults from './pages/SearchResults'; 
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/community" element={<CommunityPage />} />
-        <Route path="/producto" element={<ProductPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/search" element={<SearchResults />} />   {/* <-- agregá esto */}
-        {/* Podés dejar o quitar el index, ya que "/" ya está declarado arriba */}
-      </Routes>
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/community" element={<CommunityPage />} />
+          <Route path="/producto" element={<ProductPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/search" element={<SearchResults />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
