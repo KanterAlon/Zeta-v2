1740a9d14ddbee772f831ace3cc8329304d807c1
KanterAlon <163696462+KanterAlon@users.noreply.github.com>
2025-06-13 08:30:45 -0300

    Improve blog popup layout

--- frontend/src/components/BlogPopup.jsx
+++ frontend/src/components/BlogPopup.jsx
@@ -3,18 +3,22 @@ import React from 'react';
 const BlogPopup = ({ post, onClose }) => {
   if (!post) return null;
 
+  const handleContainerClick = (e) => {
+    if (e.target.classList.contains('pop-up-info-articulo')) onClose();
+  };
+
   return (
-    <section className="pop-up-info-articulo" style={{ display: 'flex' }}>
-      <div className="pop-up-info-articulo-inner">
-        <img src={`./img/${post.imagen_url}`} alt="Post Image" />
+    <div className="pop-up-info-articulo" onClick={handleContainerClick}>
+      <div className="pop-up-info-articulo-inner" onClick={(e) => e.stopPropagation()}>
+        <img src="./img/icon_close.svg" className="close-btn" onClick={onClose} alt="Cerrar" />
+        <img src={`./img/${post.imagen_url}`} alt="Imagen del post" />
         <h1>{post.titulo_post}</h1>
         <h3>Escrito por todo el equipo de Zeta</h3>
         <hr />
         <span>{post.fecha_creacion}</span>
         <p>{post.contenido_post}</p>
-        <button className="btn-login" onClick={onClose} style={{ marginTop: '1rem' }}>Cerrar</button>
       </div>
-    </section>
+    </div>
   );
 };
 
