6386f7c409db8d7f04278816acd2878538200841
KanterAlon <163696462+KanterAlon@users.noreply.github.com>
2025-06-19 09:55:59 -0300

    refactor: reorganize frontend pages

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import HeroSection from '../components/HeroSection';
 import InfoButtons from '../components/InfoButtons';
 import NutritionEvaluation from '../components/NutritionEvaluation';
 import Functionalities from '../components/Functionalities';
-import CommunityPosts from '../components/CommunityPosts(unused)';
+import CommunityPosts from '../components/CommunityPosts';
 import ContactSection from '../components/ContactSection';
 
 const HomePage = () => {
